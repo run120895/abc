7ff73d5a2e752df0e9226ff524678d961803068a
run120895 <run120895@gmx.net>
2018-12-13 22:41:24 +0100

    Start

---
+++ init.lua
@@ -0,0 +1,150 @@
+minetest.register_node("abc:a", {
+	description = "letter a",
+	tiles = {"a.png"}
+})
+
+minetest.register_node("abc:b", {
+	description = "letter b",
+	tiles = {"b.png"}
+})
+
+minetest.register_node("abc:c", {
+	description = "letter c",
+	tiles = {"c.png"}
+})
+
+minetest.register_node("abc:d", {
+	description = "letter d",
+	tiles = {"d.png"}
+})
+
+minetest.register_node("abc:e", {
+	description = "letter e",
+	tiles = {"e.png"}
+})
+
+minetest.register_node("abc:f", {
+	description = "letter f",
+	tiles = {"f.png"}
+})
+
+minetest.register_node("abc:g", {
+	description = "letter g",
+	tiles = {"g.png"}
+})
+
+minetest.register_node("abc:h", {
+	description = "letter h",
+	tiles = {"h.png"}
+})
+
+minetest.register_node("abc:i", {
+	description = "letter i",
+	tiles = {"i.png"}
+})
+
+minetest.register_node("abc:j", {
+	description = "letter j",
+	tiles = {"j.png"}
+})
+
+minetest.register_node("abc:k", {
+	description = "letter k",
+	tiles = {"k.png"}
+})
+
+minetest.register_node("abc:l", {
+	description = "letter l",
+	tiles = {"l.png"}
+})
+
+minetest.register_node("abc:m", {
+	description = "letter m",
+	tiles = {"m.png"}
+})
+
+minetest.register_node("abc:n", {
+	description = "letter n",
+	tiles = {"n.png"}
+})
+
+minetest.register_node("abc:o", {
+	description = "letter o",
+	tiles = {"o.png"}
+})
+
+minetest.register_node("abc:p", {
+	description = "letter p",
+	tiles = {"p.png"}
+})
+
+minetest.register_node("abc:q", {
+	description = "letter q",
+	tiles = {"q.png"}
+})
+
+minetest.register_node("abc:r", {
+	description = "letter r",
+	tiles = {"r.png"}
+})
+
+minetest.register_node("abc:s", {
+	description = "letter s",
+	tiles = {"s.png"}
+})
+
+minetest.register_node("abc:t", {
+	description = "letter t",
+	tiles = {"t.png"}
+})
+
+minetest.register_node("abc:u", {
+	description = "letter u",
+	tiles = {"u.png"}
+})
+
+minetest.register_node("abc:v", {
+	description = "letter v",
+	tiles = {"v.png"}
+})
+
+minetest.register_node("abc:w", {
+	description = "letter w",
+	tiles = {"w.png"}
+})
+
+minetest.register_node("abc:x", {
+	description = "letter x",
+	tiles = {"x.png"}
+})
+
+minetest.register_node("abc:y", {
+	description = "letter y",
+	tiles = {"y.png"}
+})
+
+minetest.register_node("abc:z", {
+	description = "letter z",
+	tiles = {"z.png"}
+})
+
+minetest.register_node("abc:down", {
+	description = "down arrow",
+	tiles = {"down.png"}
+})
+
+
+minetest.register_node("abc:up", {
+	description = "up arrow",
+	tiles = {"up.png"}
+})
+
+minetest.register_node("abc:right", {
+	description = "right arrow",
+	tiles = {"right.png"}
+})
+
+minetest.register_node("abc:left", {
+	description = "left arrow",
+	tiles = {"left.png"}
+})
