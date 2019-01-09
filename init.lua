a240fc243c16a85abee450a19b8a93f5c87c3b7e
run120895 <run120895@gmx.net>
2019-01-09 17:10:09 +0100

    alle Blöcke abbaubar machen

--- init.lua
+++ init.lua
@@ -1,159 +1,191 @@
 minetest.register_node("abc:a", {
 	description = "letter a",
-	tiles = {"a.png"}
+	tiles = {"a.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:b", {
 	description = "letter b",
-	tiles = {"b.png"}
+	tiles = {"b.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:c", {
 	description = "letter c",
-	tiles = {"c.png"}
+	tiles = {"c.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:d", {
 	description = "letter d",
-	tiles = {"d.png"}
+	tiles = {"d.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:e", {
 	description = "letter e",
-	tiles = {"e.png"}
+	tiles = {"e.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:f", {
 	description = "letter f",
-	tiles = {"f.png"}
+	tiles = {"f.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:g", {
 	description = "letter g",
-	tiles = {"g.png"}
+	tiles = {"g.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:h", {
 	description = "letter h",
-	tiles = {"h.png"}
+	tiles = {"h.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:i", {
 	description = "letter i",
-	tiles = {"i.png"}
+	tiles = {"i.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:j", {
 	description = "letter j",
-	tiles = {"j.png"}
+	tiles = {"j.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:k", {
 	description = "letter k",
-	tiles = {"k.png"}
+	tiles = {"k.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:l", {
 	description = "letter l",
-	tiles = {"l.png"}
+	tiles = {"l.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:m", {
 	description = "letter m",
-	tiles = {"m.png"}
+	tiles = {"m.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:n", {
 	description = "letter n",
-	tiles = {"n.png"}
+	tiles = {"n.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:o", {
 	description = "letter o",
-	tiles = {"o.png"}
+	tiles = {"o.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:p", {
 	description = "letter p",
-	tiles = {"p.png"}
+	tiles = {"p.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:q", {
 	description = "letter q",
-	tiles = {"q.png"}
+	tiles = {"q.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:r", {
 	description = "letter r",
-	tiles = {"r.png"}
+	tiles = {"r.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:s", {
 	description = "letter s",
-	tiles = {"s.png"}
+	tiles = {"s.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:t", {
 	description = "letter t",
-	tiles = {"t.png"}
+	tiles = {"t.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:u", {
 	description = "letter u",
-	tiles = {"u.png"}
+	tiles = {"u.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:v", {
 	description = "letter v",
-	tiles = {"v.png"}
+	tiles = {"v.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:w", {
 	description = "letter w",
-	tiles = {"w.png"}
+	tiles = {"w.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:x", {
 	description = "letter x",
-	tiles = {"x.png"}
+	tiles = {"x.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:y", {
 	description = "letter y",
-	tiles = {"y.png"}
+	tiles = {"y.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:z", {
 	description = "letter z",
-	tiles = {"z.png"}
+	tiles = {"z.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:down", {
 	description = "arrow down",
-	tiles = {"test.png"}
+	tiles = {"test.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:up", {
 	description = "arrow up",
-	tiles = {"up.png"}
+	tiles = {"up.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:right", {
 	description = "arrow right",
-	tiles = {"right.png"}
+	tiles = {"right.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:left", {
 	description = "arrow left",
-	tiles = {"left.png"}
+	tiles = {"left.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:exclamation_mark", {
 	description = "exclamation mark",
-	tiles = {"exclamation_mark.png"}
+	tiles = {"exclamation_mark.png"},
+	groups = {cracky=2}
 })
 
 minetest.register_node("abc:question_mark", {
 	description = "question_mark",
-	tiles = {"question_mark.png"}
+	tiles = {"question_mark.png"},
+	groups = {cracky=2}
 })
